5b15b9d17eb29313b8afa0f465fbafec7441af5f
Filip Ignaciuk <Ignaciuk.Filip@gmail.com>
2023-07-17 23:50:13 +0100

    Updated Text Viewer
    Updated text viewer

--- HaxelCommunication/HaxelCommunication/TextViewer.cpp
+++ HaxelCommunication/HaxelCommunication/TextViewer.cpp
@@ -1,6 +1,16 @@
-#include 
+#include <fstream>
+#include <map>
 
-class TextViewer
+
+static class TextViewer
 {
-
+private:
+	static std::string m_fileName;
+	static std::fstream m_file;
+public:
+	static const enum readType { Normal, Config };
+	static std::string ReadFile(std::string _path, readType _readType)
+	{
+		m_file.open(_path);
+	}
 };
