61cfca1652e7f3ceae6e5cdd24044fae58526aa0
Filip Ignaciuk <Ignaciuk.Filip@gmail.com>
2023-08-19 22:49:16 +0100

    Added server files

---
+++ Server/Server/user.hpp
@@ -0,0 +1,24 @@
+
+#pragma once
+#include <string>
+
+class IDGenerator
+{
+	static int GenerateID();
+};
+
+class User
+{
+private:
+	std::string id;
+	std::string displayName;
+
+public:
+	User(std::string _displayName, std::string _id);
+
+
+	std::string GetId() const;
+	std::string GetDisplayName() const;
+	void SetDisplayName(std::string _displayName);
+
+};
