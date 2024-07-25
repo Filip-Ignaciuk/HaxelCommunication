ed66bc3f88e1f42d86ca23c952b9bccba4c7d5f4
Filip Ignaciuk <Ignaciuk.Filip@gmail.com>
2024-07-25 15:48:12 +0100

    Work on error handler

--- Client/Client/ErrorHandler.hpp
+++ Client/Client/ErrorHandler.hpp
@@ -8,10 +8,32 @@ class ErrorHandler
 private:
 	static bool hasErrors;
 	static std::queue<Error> m_errors;
+	
+	// For Questions
+	static bool hasResponded;
+	static Error responseError;
+
+	static bool responseBool;
+	static std::string responseString;
 
 public:
 	static void AddError(const Error& _error);
 	static Error GetError();
 	static void DeleteError();
 	static bool HasError();
+
+	// For Questions
+	static bool GetHasResponded();
+	static bool SetHasResponded(bool _hasResponded);
+
+	static Error& GetCurrentResponseError();
+	static Error& SetCurrentResponseError(Error& _currentResponseError);
+
+	static bool GetResponseBool();
+	static bool SetResponseBool(bool _responseBool);
+	
+	static std::string& GetResponseString();
+	static std::string& SetResponseString(std::string& _responseString);
+
+	static void ClearCurrentResponse();
 };
